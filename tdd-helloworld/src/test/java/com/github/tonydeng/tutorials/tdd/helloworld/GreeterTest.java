752f0bea33f3739dc6c74ba64a4715818c26ebda
Tony Deng <wolf.deng@gmail.com>
2018-09-13 00:53:12 +0800

    :white_check_mark: test(1.0): 添加SayHello测试
    refs #1
    
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

---
+++ tdd-helloworld/src/test/java/com/github/tonydeng/tutorials/tdd/helloworld/GreeterTest.java
@@ -0,0 +1,13 @@
+package com.github.tonydeng.tutorials.tdd.helloworld;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class GreeterTest {
+
+    @Test
+    public void testShouldSayHelloToTheWorld() {
+        log.info("test");
+    }
+}
