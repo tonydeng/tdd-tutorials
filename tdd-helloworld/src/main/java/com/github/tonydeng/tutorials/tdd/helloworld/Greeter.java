a4ce08d7221bd534fd2db8dfd6c8591134ea9ef4
Tony Deng <wolf.deng@gmail.com>
2018-09-13 00:58:58 +0800

    :sparkles: feat(1.0): 添加Greeter的SayHello方法
    refs #1
    
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

---
+++ tdd-helloworld/src/main/java/com/github/tonydeng/tutorials/tdd/helloworld/Greeter.java
@@ -0,0 +1,7 @@
+package com.github.tonydeng.tutorials.tdd.helloworld;
+
+public class Greeter {
+    String sayHello() {
+        return "Hello World";
+    }
+}
--- tdd-helloworld/src/test/java/com/github/tonydeng/tutorials/tdd/helloworld/GreeterTest.java
+++ tdd-helloworld/src/test/java/com/github/tonydeng/tutorials/tdd/helloworld/GreeterTest.java
@@ -1,13 +1,20 @@
 package com.github.tonydeng.tutorials.tdd.helloworld;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class GreeterTest {
 
+    private static Greeter greeter;
+
+    public static void init() {
+        greeter = new Greeter();
+    }
+
     @Test
     public void testShouldSayHelloToTheWorld() {
-        log.info("test");
+        Assertions.assertNotNull(greeter.sayHello());
     }
 }
